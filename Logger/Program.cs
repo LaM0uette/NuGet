258660da4d9bd7341c7a9fac16148841358c5772
LaM0uette <lam0uette.dev@gmail.com>
2022-07-08 14:49:02 +0200

    automatisation log.info

--- Logger/Program.cs
+++ Logger/Program.cs
@@ -7,10 +7,19 @@
         private string Directory { get; }
 
         private string LogName { get; }
-        
+
         private string FileName { get; }
 
         private string FilePath { get; }
+
+        //
+        // Class
+        public enum TypeLog
+        {
+            All = 0,
+            Cmd = 1,
+            Log = 2,
+        }
 
         //
         // Functions
@@ -18,30 +27,50 @@
         {
             return value.ToString("yyyy-MM-dd__HH-mm-ss");
         }
-        
+
         public Log(string dir = "", string log = "")
         {
             var timeStamp = GetTimestamp(DateTime.Now);
-            
+
             Directory = dir == "" ? System.IO.Directory.GetCurrentDirectory() : dir;
             LogName = log == "" ? LogName = "Log" : log;
             FileName = LogName + $"_{timeStamp}.log";
             FilePath = Path.Join(Directory, FileName);
         }
-        
+
+        public void WriteLog(string msg)
+        {
+            using var w = File.AppendText(FilePath);
+            {
+                w.WriteLine(msg);
+            }
+        }
+
         //
         // Loging
-        public void Info(string message)
+        public void Base(string msg)
         {
-            Console.WriteLine("Logged : {0}", message);
+            Console.WriteLine("Logged : {0}", msg);
 
             using (var w = File.AppendText(FilePath))
             {
                 w.Write("\r\nLog Entry : ");
                 w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
-                w.WriteLine("  :{0}", message);
+                w.WriteLine("  :{0}", msg);
                 w.WriteLine("-----------------------------------------------");
             }
+        }
+
+        public void Info(string msg, TypeLog typeLog = TypeLog.All)
+        {
+            if (typeLog != TypeLog.Log)
+            {
+                Console.WriteLine("Logged : {0}", msg);
+            }
+
+            var msgFormat = $"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()} : {msg}";
+
+            if (typeLog != TypeLog.Cmd) WriteLog(msgFormat);
         }
     }
 }
