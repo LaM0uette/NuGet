a4b27dbf75972e0e77dac701de0197a5df147e98
LaM0uette <lam0uette.dev@gmail.com>
2022-07-12 15:59:43 +0200

    up

--- Logger/obj/Debug/net6.0/Logger.AssemblyInfo.cs
+++ Logger/obj/Debug/net6.0/Logger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("© 2022 - 2030 LaM0uette")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("Gestions des logs automatisés")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Gestion des logs automatisés")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logger")]
