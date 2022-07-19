4de305a91c1b695fdce59749ac8a2c890bfe3532
LaM0uette <lam0uette.dev@gmail.com>
2022-07-19 08:52:34 +0200

    up

--- CommonTasks/obj/Debug/net6.0/CommonTasks.AssemblyInfo.cs
+++ CommonTasks/obj/Debug/net6.0/CommonTasks.AssemblyInfo.cs
@@ -12,11 +12,14 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonTasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("© 2022 - 2030 LaM0uette")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Fonctions générales à de multiples applications")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonTasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonTasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/LaM0uette/NuGet")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
 
